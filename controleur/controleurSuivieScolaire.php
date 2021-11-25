0e1867bf49698f273f146c31d2eaeb58d473d445
SarouleLamoule <chaussat.tim@gmail.com>
2021-11-25 16:54:06 +0100

    page diplome terminée

--- controleur/controleurSuivieScolaire.php
+++ controleur/controleurSuivieScolaire.php
@@ -6,15 +6,24 @@ $SuivieScolaire = new Formulaire('post', 'index.php', 'fSuivieScolaire', 'fSuivi
 $SuivieScolaire->ajouterComposantLigne($SuivieScolaire->debutDiv("BodySuivieScolaire"));
     $SuivieScolaire->ajouterComposantLigne($SuivieScolaire->debutDiv("SelectSuivieScolaire"));
         $SuivieScolaire->ajouterComposantLigne($SuivieScolaire->creerLabel("AnneeUniversitaire", "AnneeUniversitaire", "Année universitaire: "));
-        $SuivieScolaire->ajouterComposantLigne($SuivieScolaire->creerSelect("selectAnnee","selectAnnee",["1er année","2eme année","3eme année"],""));
+        $SuivieScolaire->ajouterComposantLigne($SuivieScolaire->creerSelect("selectAnnee","selectAnnee",["3eme année","1er année","2eme année"],""));
     $SuivieScolaire->ajouterComposantLigne($SuivieScolaire->finDiv());
 
     $SuivieScolaire->ajouterComposantLigne($SuivieScolaire->debutDiv("SuivieScolaireDiplomes"));
-        // $SuivieScolaire->ajouterComposantLigne($SuivieScolaire->debutDiv("AnneeUniversitaireBlock"));
-        //     $SuivieScolaire->ajouterComposantLigne($SuivieScolaire-> creerLabel("LabelDocument", "LabelDocument", "Diplome"));
-        // $SuivieScolaire->ajouterComposantLigne($SuivieScolaire->finDiv());
+        // $SuivieScolaire->ajouterComposantLigne($SuivieScolaire->creerLabel("DiplômeUniversitaire", "DiplômeUniversitaire", "Selectionnez le document que vous voulez: "));
+        // $SuivieScolaire->ajouterComposantLigne($SuivieScolaire->creerSelect("selectAnnee","selectAnnee",["Diplôme","Bulletin 1er semestre","Bulletin 2eme semestre"],""));
+        $SuivieScolaire->ajouterComposantLigne($SuivieScolaire->creerInputSubmit("semestre1", "semestre1", "1er semestre", ""));
+        $SuivieScolaire->ajouterComposantLigne($SuivieScolaire->creerInputSubmit("semestre2", "semestre2", "2eme semestre", ""));
+        $SuivieScolaire->ajouterComposantLigne($SuivieScolaire->creerInputSubmit("diplome", "diplome", "Diplôme", ""));
     $SuivieScolaire->ajouterComposantLigne($SuivieScolaire->finDiv());
 
+    $SuivieScolaire->ajouterComposantLigne($SuivieScolaire->debutDiv("SuivieScolaireDocument"));
+        $SuivieScolaire->ajouterComposantLigne($SuivieScolaire->creerImage("images\Diplome-licence-france.png"));
+    $SuivieScolaire->ajouterComposantLigne($SuivieScolaire->finDiv());
+
+    $SuivieScolaire->ajouterComposantLigne($SuivieScolaire->debutDiv("SuivieScolaireTelecharger"));
+        $SuivieScolaire->ajouterComposantLigne($SuivieScolaire->creerInputSubmit("TelechargerDocument", "TelechargerDocument", "Telecharger le document", ""));
+    $SuivieScolaire->ajouterComposantLigne($SuivieScolaire->finDiv());
 
 $SuivieScolaire->ajouterComposantLigne($SuivieScolaire->finDiv());
 
